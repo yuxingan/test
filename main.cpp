abb4214c8cfe8a19aebdcc9e90fc9950d1814a60
yuxingan <3174227646@qq.com>
2021-08-03 11:12:34 +0800

    vscode test

--- main.cpp
+++ main.cpp
@@ -7,14 +7,3 @@ int main ()
 	getchar();
 	return 0;
 }
-int fun();
-int testfun();
-int testwofun();
-int test2fun();
-void test();
-void fun();
-float fun();
-double fun();
-char fun();
-unsigned fun();
-short fun();
