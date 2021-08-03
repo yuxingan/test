96b24a7bb058230ab9fa52acddf0d32bb7d3171b
yuxingan <3174227646@qq.com>
2021-08-03 11:07:14 +0800

    vscode test

--- main.cpp
+++ main.cpp
@@ -4,15 +4,6 @@ int main ()
 {
 	std::cout << "hello" << std::endl;
 	std::cout << "world" << std::endl;
+	getchar();
 	return 0;
 }
-int fun();
-int testfun();
-int testwofun();
-void test();
-void fun();
-float fun();
-double fun();
-char fun();
-unsigned fun();
-short fun();
